dca597608aff516dac2f7a0219d8827a5ba90228
AthangDeshmukh <athang.deshmukh@gmail.com>
2025-05-22 19:47:25 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Product from "./components/Product";
-import Cart from "./components/Cart";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <header>
-          <h1>My Online Shop</h1>
-          <Link to="/">Home</Link>-
-          <Link to="/cart">Cart</Link>
-          <hr />
-        </header>
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+// import Product from "./components/Product";
+// import Cart from "./components/Cart";
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// function App() {
+//   return (
+//     <div>
+//       <BrowserRouter>
+//         <header>
+//           <h1>My Online Shop</h1>
+//           <Link to="/">Home</Link>-
+//           <Link to="/cart">Cart</Link>
+//           <hr />
+//         </header>
 
-        <main>
-          <Routes>
-            <Route index element={<Product />} />
-            <Route path="/" element={<Product/>}></Route>
-            <Route path="/cart" element={<Cart/>}></Route>
-          </Routes>
-        </main>
+//         <main>
+//           <Routes>
+//             <Route index element={<Product />} />
+//             <Route path="/" element={<Product/>}></Route>
+//             <Route path="/cart" element={<Cart/>}></Route>
+//           </Routes>
+//         </main>
 
-        <footer>
-          <hr />
-          &copy; 2005. All rights Reserved.
-        </footer>
-      </BrowserRouter>
-    </div>
-  );
-}
-export default App;
+//         <footer>
+//           <hr />
+//           &copy; 2005. All rights Reserved.
+//         </footer>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+// export default App;
